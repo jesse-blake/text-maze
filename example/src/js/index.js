4a8b271dff9cc6313d7c3fe7a8ec6910c6774197
Jesse Blake <jesse@littlesystem.com>
2017-03-25 14:48:31 -0500

    Add controls, add bootstrap dropdowns to example
        - Add controls to change colors and toggle color fluctuation.
        - Add bootstrap dropdowns to nest controls in the example.

--- example/src/js/index.js
+++ example/src/js/index.js
@@ -1,10 +1,6 @@
 var textMaze = require('text-maze');
 
 textMaze.init({
-    currentlyShowingText: true,
     useSpeedMeter: true,
-    colors: {
-        randomize: true,
-        fluctuate: true
-    }
+    fluctuateSolutionColor: true
 });
