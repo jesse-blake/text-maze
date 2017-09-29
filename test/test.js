d839a59daaea99e704b324d7556129f9d8e1ec25
Jesse Blake <jesse@littlesystem.com>
2017-09-29 18:06:20 -0500

    Take width and margin as options to text maze init

--- test/test.js
+++ test/test.js
@@ -8,9 +8,36 @@ describe('maze config: user params:', function() {
     const textMaze = require('text-maze');
 
     it('"text" is required and should be a non-zero length string', function() {
-        expect(() => { textMaze.init(); }).to.throw(TypeError, 'option "text"');
-        expect(() => { textMaze.init(43); }).to.throw(TypeError, 'option "text"');
-        expect(() => { textMaze.init(''); }).to.throw(TypeError, 'option "text"');
+        expect(() => { textMaze.init(); }).to.throw(TypeError, 'arg "text"');
+        expect(() => { textMaze.init(43); }).to.throw(TypeError, 'arg "text"');
+        expect(() => { textMaze.init(''); }).to.throw(TypeError, 'arg "text"');
+    });
+
+    it('"options" must be an object or undefined', function() {
+        expect(() => { textMaze.init('foo'); }).to.not.throw(TypeError, 'arg "options"');
+        expect(() => { textMaze.init('foo', {}); }).to.not.throw(TypeError, 'arg "options"');
+
+        expect(() => { textMaze.init('foo', true); }).to.throw(TypeError, 'arg "options"');
+        expect(() => { textMaze.init('foo', 43); }).to.throw(TypeError, 'arg "options"');
+        expect(() => { textMaze.init('foo', null); }).to.throw(TypeError, 'arg "options"');
+    });
+
+    it('"options" must fail on unrecognized options', function() {
+        expect(() => { textMaze.init('foo', {foo: 43}); }).to.throw(TypeError, 'unrecognized option');
+    });
+
+    it('"options.width" must be a valid integer', function() {
+        expect(() => { textMaze.init('foo', {width: 19}); }).to.throw(TypeError, 'option "width"');
+        expect(() => { textMaze.init('foo', {width: null}); }).to.throw(TypeError, 'option "width"');
+        expect(() => { textMaze.init('foo', {width: true}); }).to.throw(TypeError, 'option "width"');
+        expect(() => { textMaze.init('foo', {width: 'foo'}); }).to.throw(TypeError, 'option "width"');
+    });
+
+    it('"options.margin" must be a valid integer', function() {
+        // Not testing invalid integers: window.innerWidth is unavailable.
+        expect(() => { textMaze.init('foo', {margin: null}); }).to.throw(TypeError, 'option "margin"');
+        expect(() => { textMaze.init('foo', {margin: true}); }).to.throw(TypeError, 'option "margin"');
+        expect(() => { textMaze.init('foo', {margin: 'foo'}); }).to.throw(TypeError, 'option "margin"');
     });
 });
 
